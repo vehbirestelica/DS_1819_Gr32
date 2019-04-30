23c29526c7f874a6bd042ce9ed520ea2593be9a2
Vehbi Restelica <34625772+vehbirestelica@users.noreply.github.com>
2019-04-30 21:37:24 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace foursquarecypher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plainText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cipherText_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
